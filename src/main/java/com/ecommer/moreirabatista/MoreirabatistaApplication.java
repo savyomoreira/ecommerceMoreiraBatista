5f17480ce2ad0154967496ed26f2a1092e273fcb
savyomoreira <savyo_21@hotmail.com>
2020-08-22 17:11:41 -0300

    init

---
+++ src/main/java/com/ecommer/moreirabatista/MoreirabatistaApplication.java
@@ -0,0 +1,13 @@
+package com.ecommer.moreirabatista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreirabatistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreirabatistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommer/moreirabatista/MoreirabatistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommer.moreirabatista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreirabatistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
